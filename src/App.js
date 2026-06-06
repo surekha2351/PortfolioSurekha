c91f1893e2e654f7db7548e41dc0bc0492b31baa
surekha2351 <yarabattisurekha3260@gmail.com>
2026-06-06 09:56:17 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
-import { useState, useEffect } from "react";
-import { darkTheme, lightTheme } from './utils/Themes.js'
+import { useState } from "react";
+import { darkTheme, lightTheme } from './utils/Themes.js';
 import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,7 +11,7 @@ import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
-import Services from "./components/Services"
+import Services from "./components/Services";
 import Certificates from "./components/Certificates";
 import Education from "./components/Education";
 import ProjectDetails from "./components/ProjectDetails";
@@ -21,20 +21,22 @@ const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
   overflow-x: hidden;
-`
+`;
 
 const Wrapper = styled.div`
   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
   width: 100%;
-  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
-`
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
+
 function App() {
   const [darkMode, setDarkMode] = useState(true);
   const [openModal, setOpenModal] = useState({ state: false, project: null });
-  console.log(openModal)
+
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-      <Router >
+      {/* basename add chesam, idi GitHub Pages kosam */}
+      <Router basename="/PortfolioSurekha">
         <Navbar />
         <Body>
           <HeroSection />
@@ -43,8 +45,8 @@ function App() {
             <Experience />
           </Wrapper>
           <Projects openModal={openModal} setOpenModal={setOpenModal} />
-          <Services/>
-          <Certificates/>
+          <Services />
+          <Certificates />
           <Wrapper>
             <Education />
             <Contact />
@@ -53,7 +55,6 @@ function App() {
           {openModal.state &&
             <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
           }
-        
         </Body>
       </Router>
     </ThemeProvider>
